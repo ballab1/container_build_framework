c7323cd52bd4af6fb153dae0d179300a8f2d1f41
Bob Ballantyne <bob.ballantyne@emc.com>
2018-02-11 10:45:10 -0500

    fixed error msg when empty dirs found

--- 07.permissions/01.bin_dirs.sh
+++ 07.permissions/01.bin_dirs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 function permissions.set_bindirs()
 {
@@ -7,8 +7,9 @@ function permissions.set_bindirs()
     for dir in ${dirlist}; do
         mkdir -p "$dir" && chmod -R 777 "$dir"
         local -a files=( $( ls "$dir"/* 2> /dev/null ) )
-        [ ${#files[@]} -gt 0 ] && chmod 755 "${files[*]}"
+        [ ${#files[@]} -gt 0 ] && chmod 755 "$dir"/*
     done
+    true
 }
 
 permissions.set_bindirs '/usr/local/bin /usr/bin'
