d408627ef3b48fff0e609e0c039da5d380f99853
Bob Ballantyne <bob.ballantyne@emc.com>
2018-02-10 19:16:29 -0500

    set files executabes

--- 07.permissions/01.bin_dirs.sh
+++ 07.permissions/01.bin_dirs.sh
@@ -5,7 +5,8 @@ function permissions.set_bindirs()
     local -r dirlist=$1
     
     for dir in ${dirlist}; do
-        mkdir -p ${dir} && chmod -R 777 ${dir}
+        mkdir -p "${dir}" && chmod -R 777 "${dir}"
+        chmod 755 "${dir}"/*
     done
 }
 
