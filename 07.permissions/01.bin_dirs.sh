102fb146e888255cfb8a4f17a1ca32974583e060
Bob Ballantyne <bob.ballantyne@emc.com>
2018-02-17 08:33:57 -0500

    updated 07.permissions/01.bin_dirs.sh

--- 07.permissions/01.bin_dirs.sh
+++ 07.permissions/01.bin_dirs.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-function permissions.set_bindirs()
-{
-    local -r dirlist=$1
-    
-    for dir in ${dirlist}; do
-        mkdir -p "$dir" && chmod -R 777 "$dir"
-        local -a files=( $( ls "$dir"/* 2> /dev/null ) )
-        [ ${#files[@]} -gt 0 ] && chmod 755 "$dir"/*
-    done
-    true
-}
+source "${TOOLS}/01.bashlib/lib.sh"
 
-permissions.set_bindirs '/usr/local/bin /usr/bin'
+declare -r dirlist='/usr/local/bin /usr/bin /sbin'
+
+for dir in ${dirlist}; do
+    mkdir -p "$dir" && chmod -R 777 "$dir"
+    declare -a files=( $( lib.getFiles "$dir" ) )
+    [ ${#files[@]} -gt 0 ] && chmod 755 "$dir"/*
+done
+true
