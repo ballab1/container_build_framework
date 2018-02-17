aa63729a1fdf0b3c41569fdd59b6031dce867fb2
Bob Ballantyne <bob.ballantyne@emc.com>
2018-02-17 09:40:45 -0500

    allow all root folder to get custom content

--- 06.customizations/01.custom_folders.sh
+++ 06.customizations/01.custom_folders.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-function customizations.custom_folders()
-{
-    local -r tools=$1
-    local -r dirs=$2
-    
-    for dir in ${dirs} ; do
-        [ -d "${tools}/${dir}" ] && cp -r "${tools}/${dir}/"* "/${dir}/"
-    done
-    true
-}
+declare -r tools=/tmp
+declare -r dirs='bin etc home lib lib64 media mnt opt root sbin usr var'
 
-customizations.custom_folders /tmp 'etc usr opt var'
+for dir in ${dirs} ; do
+    [ -d "${tools}/${dir}" ] && cp -r "${tools}/${dir}/"* "/${dir}/"
+done
+
+true
