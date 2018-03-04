2e87d5dbc6ded69288d8c3338c4a0733a8ae833a
Bob Ballantyne <bob.ballantyne@emc.com>
2018-03-04 16:42:38 -0500

    add run framework

--- project.templates/build/usr/local/bin/docker-entrypoint.sh
+++ project.templates/build/usr/local/bin/docker-entrypoint.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 declare -r config_entry='newcontainer-setup' 
-declare -r tools=/usr/local/bin
-source "${tools}/docker.helper"
+declare -r tools=/usr/local/crf
+source "${tools}/init.runtime"
 docker.setExports
 
 
 if [[ "$1" = 'newcontainer' ]]; then
     # this is the primary (default) codepath invoked by the Dockerfile
     printf "\e[32m>>>>>>>> entering \e[33m'%s'\e[0m\n" "$1"
-    sudo -E "$0" "$config_entry"
-    /sbin/tini -s -v -- "${tools}/run.sh"
+    [ "$( which sudo )" ] && sudo -E "$0" "$config_entry"
+    [ ! -e "${tools}/run.sh" ] && trap.die 'No run.sh detected'
+    exec "${tools}/run.sh"
 
 elif [[ "$1" = "$config_entry" && "$(id -u)" -eq 0 ]]; then
     # this codepath is invoked (from above) to perpare the runtime environment. User is 'root' so chmod & chown succeed
