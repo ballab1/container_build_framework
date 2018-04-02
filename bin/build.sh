72351ff81c9d360b847206ba07fe348c0eeb5d8c
Ballantyne, Bob <bob.ballantyne@emc.com>
2018-04-02 15:06:50 -0400

    measure & display build time

--- bin/build.sh
+++ bin/build.sh
@@ -6,12 +6,15 @@ set -o nounset
 declare NAME=${1:?'Input parameter "NAME" must be defined'} 
 declare TZ="${2:-null}"
 
-if [ "$(pwd)" = '/tmp' ]; then
-    # load our libraries
-    source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/init.libraries"
-
-    # build our container
-    cbf.buildContainer "$NAME" "$TZ"
-else
+if [ "$(pwd)" != '/tmp' ]; then
     echo "This script should only be run from a container build environment"
+    exit 1
 fi
+
+# load our libraries
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/init.libraries"
+
+# build our container
+timer.measureCmd "$NAME" 'cbf.buildContainer' "$NAME" "$TZ"
+echo ''
+echo ''
