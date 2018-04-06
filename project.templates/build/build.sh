6f4a087592b3d9cd80cd92f4c3a5303356405a25
Ballantyne, Bob <bob.ballantyne@emc.com>
2018-04-06 10:52:28 -0400

    move retrieval of CBF to build.sh and have it conditional

--- project.templates/build/build.sh
+++ project.templates/build/build.sh
@@ -4,12 +4,15 @@ cd /tmp
 declare cbf_dir=/tmp/container_build_framework
 
 if [ ! -d "$cbf_dir" ]; then
-
-    declare cbf_tar=$( ls *.tar.gz )
-    [ -z "$cbf_tar" ] || tar xzf "${cbf_tar}"
-
+    # since no CBF directory located, attempt to download CBF based on specified verion
+    declare CBF_URL="https://github.com/ballab1/container_build_framework/archive/${CBF_VERSION}.tar.gz"         
+    wget --no-check-certificate --quiet --output-document=- "$CBF_URL" | tar -xz
     cbf_dir="$( ls -d container_build_framework-* )"
-    [ "$cbf_dir" ] || exit 1
 fi
+if [ -z "$cbf_dir" ] || [ ! -d "$cbf_dir" ]; then
+    echo 'No framework directory located'
+    exit 1
+fi
+
 echo "loading framework from ${cbf_dir}"
 source "${cbf_dir}/bin/build.sh" "$@"
