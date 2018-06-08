c3b3f7eaac81abbed6eb558ff7d7d043f582daf2
Ballantyne, Bob <bob.ballantyne@emc.com>
2018-06-08 10:51:51 -0400

    crf updates

--- project.templates/build/build.sh
+++ project.templates/build/build.sh
@@ -13,7 +13,8 @@ if [ ! -d "$cbf_dir" ] && [ "${CBF_VERSION}" ]; then
     # since no CBF directory located, attempt to download CBF based on specified verion
     CBF_URL="https://github.com/ballab1/container_build_framework/archive/${CBF_VERSION}.tar.gz"
     wget --no-check-certificate --quiet --output-document=- "$CBF_URL" | tar -xz
-    cbf_dir="$( ls -d container_build_framework-* )"
+    [ $? -eq 0 ] || exit 1
+    cbf_dir="$( ls -d container_build_framework-* 2>/dev/null )"
 fi
 
 CBF_TGZ=/usr/local/crf/cbf.tar.gz
