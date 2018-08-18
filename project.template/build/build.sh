1e9b2a270c42b4067e115c295365189e23e19148
Bob Ballantyne <ballab1@users.noreply.github.com>
2018-08-18 10:33:41 -0400

    fix build.sh (#11)

--- project.template/build/build.sh
+++ project.template/build/build.sh
@@ -25,24 +25,37 @@ cd /tmp
 cbf_dir=/tmp/container_build_framework
 
 # check if we need to download CBF
-if [ ! -d "$cbf_dir" ] && [ "$CBF_VERSION" ]; then
-    CBF_URL="https://github.com/ballab1/container_build_framework/archive/${CBF_VERSION}.tar.gz"
-    echo "Downloading CBF:$CBF_VERSION from $CBF_URL"
+if [ -d "$cbf_dir" ]; then
+    echo "Using local build version of CBF"
+
+elif [ "$CBF_VERSION" ]; then
     # since no CBF directory located, attempt to download CBF based on specified verion
     CBF_TGZ=/tmp/cbf.tar.gz
+    CBF_URL="https://github.com/ballab1/container_build_framework/archive/${CBF_VERSION}.tar.gz"
+    echo "Downloading CBF:$CBF_VERSION from $CBF_URL"
+
     wget --no-check-certificate --quiet --output-document="$CBF_TGZ" "$CBF_URL" || die "Failed to download $CBF_URL"
-    
+    if type -f wget &> /dev/null ; then
+        wget --no-check-certificate --quiet --output-document="$CBF_TGZ" "$CBF_URL" || die "Failed to download $CBF_URL"
+    elif type -f curl &> /dev/null ; then
+        curl --insecure --silent --output "$CBF_TGZ" "$CBF_URL" || die "Failed to download $CBF_URL"
+    else
+        die "Neither wget or curl is installed to download cbf from $CBF_URL"
+    fi
+
     echo 'Unpacking downloaded copy of CBF'
     tar -xzf "$CBF_TGZ" || die "Failed to unpack $CBF_TGZ"
     cbf_dir="$( ls -d container_build_framework* 2>/dev/null )"
 
 else
+    echo 'Unpacking stashed copy of CBF'
+
     # setup pointer to archive from prior build
     CBF_TGZ=/usr/local/crf/cbf.tar.gz
     mkdir -p "$cbf_dir" || die 'Unable to unpack stashed CBF'
-    echo 'Unpacking stashed copy of CBF'
     tar -xzf "$CBF_TGZ" -C "$cbf_dir" || die "Failed to unpack $CBF_TGZ"
 fi
+
 
 # verify CBF directory exists
 [ "$cbf_dir" ] && [ -d "$cbf_dir" ] ||  die 'No framework directory located'
