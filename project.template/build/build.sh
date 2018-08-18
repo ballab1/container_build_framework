d736e90fd3d6c44d9404102f97384d21d203f4a3
Ballantyne, Bob <bob.ballantyne@emc.com>
2018-08-18 00:38:44 +0000

    fix build.sh

--- project.template/build/build.sh
+++ project.template/build/build.sh
@@ -25,26 +25,32 @@ cd /tmp
 cbf_dir=/tmp/container_build_framework
 
 # check if we need to download CBF
-if [ ! -d "$cbf_dir" ] && [ "$CBF_VERSION" ]; then
-    CBF_URL="https://github.com/ballab1/container_build_framework/archive/${CBF_VERSION}.tar.gz"
-    echo "Downloading CBF:$CBF_VERSION from $CBF_URL"
+if [ -d "$cbf_dir" ]; then
+    echo "Using local build version of CBF"
+
+elif [ "$CBF_VERSION" ]; then
+    echo "Downloading CBF:$CBF_VERSION from github"
+
     # since no CBF directory located, attempt to download CBF based on specified verion
     CBF_TGZ=/tmp/cbf.tar.gz
+    CBF_URL="https://github.com/ballab1/container_build_framework/archive/${CBF_VERSION}.tar.gz"
     wget --no-check-certificate --quiet --output-document="$CBF_TGZ" "$CBF_URL" || die "Failed to download $CBF_URL"
-    
+
     echo 'Unpacking downloaded copy of CBF'
     tar -xzf "$CBF_TGZ" || die "Failed to unpack $CBF_TGZ"
-    cbf_dir="$( ls -d container_build_framework* 2>/dev/null )"
+    cbf_dir="$( ls -d container_build_framework-* 2>/dev/null )"
 
 else
+    echo 'Unpacking stashed copy of CBF'
+
     # setup pointer to archive from prior build
     CBF_TGZ=/usr/local/crf/cbf.tar.gz
     mkdir -p "$cbf_dir" || die 'Unable to unpack stashed CBF'
-    echo 'Unpacking stashed copy of CBF'
     tar -xzf "$CBF_TGZ" -C "$cbf_dir" || die "Failed to unpack $CBF_TGZ"
 fi
 
-# verify CBF directory exists
+
+# vereify CBF directory exists
 [ "$cbf_dir" ] && [ -d "$cbf_dir" ] ||  die 'No framework directory located'
 
 
