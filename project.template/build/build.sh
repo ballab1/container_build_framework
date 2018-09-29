27cb325aedeb3c42c760c39772c0db4f0aba3002
Ballantyne, Bob <bob.ballantyne@emc.com>
2018-09-29 18:45:32 +0000

    changed OS to CONTAINER_OS

--- project.template/build/build.sh
+++ project.template/build/build.sh
@@ -7,9 +7,9 @@ die() {
 
 osname() {
     if [ -e /etc/os-release ]; then
-        cat /etc/os-release | grep -E '^ID=' 2>/dev/null | awk -F '=' '{print $2}'
+        cat /etc/os-release | grep -E '^ID=' 2>/dev/null | awk -F '=' '{print $2}' | tr -d '"'
     else
-        echo mingw64
+        die 'Unsupported OS - no /etc/os-release detected.'
     fi
 }
 
@@ -24,28 +24,30 @@ echo '====================================================='
 case "$(osname)" in
     alpine)
         # ensure we have bash support
-        if [ -z "$(which bash)" ]; then
+        if [ -z "$(type -P bash)" ]; then
             apk update
             apk add --no-cache bash ca-certificates openssl
         fi;;
     centos)
         # ensure we have wget support
-        if [ -z "$(which wget)" ]; then
-            yum update
-            yum install -y wget
+        if [ -z "$(type -P wget)" ]; then
+            yum update -y
+            yum install -y wget openssl
         fi;;
     fedora)
         # ensure we have wget support
-        if [ -z "$(which wget)" ]; then
-            dnf update
-            dnf install -y wget
+        if [ -z "$(type -P wget)" ]; then
+            yum update -y
+            yum install -y wget findutils
         fi;;
     ubuntu)
         # ensure we have wget support
-        if [ -z "$(which wget)" ]; then
+        if [ "$(type wget)" = 'wget: not found' ]; then
             apt-get update
-            apt-get install -y apt-utils wget
+            apt-get install -y apt-utils wget ca-certificates openssl
         fi;;
+    *)
+        die "'$(osname)' is not supported at this time";;
 esac
 
 cd /tmp
@@ -54,6 +56,7 @@ cbf_dir=/tmp/container_build_framework
 # check if we need to download CBF
 if [ -d "$cbf_dir" ]; then
     echo "Using local build version of CBF"
+    find /tmp -name 'C?F.properties' -delete
 
 elif [ "$CBF_VERSION" ]; then
     # since no CBF directory located, attempt to download CBF based on specified verion
@@ -61,10 +64,10 @@ elif [ "$CBF_VERSION" ]; then
     CBF_URL="https://github.com/ballab1/container_build_framework/archive/${CBF_VERSION}.tar.gz"
     echo "Downloading CBF:$CBF_VERSION from $CBF_URL"
 
-    if [ $(which wget) ]; then
+    if [ "$(type wget)" ]; then
         wget --no-check-certificate --quiet --output-document="$CBF_TGZ" "$CBF_URL" || die "Failed to download $CBF_URL"
 
-    elif [ $(which curl) ]; then
+    elif [ "$(type curl)" ]; then
         curl --insecure --silent --output "$CBF_TGZ" "$CBF_URL" || die "Failed to download $CBF_URL"
 
     else
@@ -72,7 +75,7 @@ elif [ "$CBF_VERSION" ]; then
     fi
 
     echo 'Unpacking downloaded copy of CBF'
-    tar -xzf "$CBF_TGZ" || die "Failed to unpack $CBF_TGZ"
+    tar --exclude 'C?F.properties' -xzf "$CBF_TGZ" || die "Failed to unpack $CBF_TGZ"
     cbf_dir="$( ls -d container_build_framework* 2>/dev/null )"
 
 else
@@ -81,7 +84,7 @@ else
     # setup pointer to archive from prior build
     CBF_TGZ=/usr/local/crf/cbf.tar.gz
     mkdir -p "$cbf_dir" || die 'Unable to unpack stashed CBF'
-    tar -xzf "$CBF_TGZ" -C "$cbf_dir" || die "Failed to unpack $CBF_TGZ"
+    tar --exclude 'C?F.properties'  -xzf "$CBF_TGZ" -C "$cbf_dir" || die "Failed to unpack $CBF_TGZ"
 fi
 
 
